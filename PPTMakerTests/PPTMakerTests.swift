8656c0eb3b32046c3b180987caa446012ac86fb3
Kimbert Bartiquel <bartiquelk@gmail.com>
2025-11-19 12:25:08 +0800

    Initial Commit

---
+++ PPTMakerTests/PPTMakerTests.swift
@@ -0,0 +1,16 @@
+//
+//  PPTMakerTests.swift
+//  PPTMakerTests
+//
+//  Created by Kimbert Bartiquel on 11/19/25.
+//
+
+import Testing
+
+struct PPTMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
